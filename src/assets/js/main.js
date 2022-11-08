d2a6d5ef556a4550bccce1a5f81dcc63043745e9
Brayan Esteves <brayan.esteves@insoftar.com>
2022-11-08 15:04:41 -0400

    ✅ CSS Global [08.11.2022] ✅

---
+++ src/assets/js/main.js
@@ -0,0 +1,39 @@
+/*<SHOW SIDEBAR>*/
+/*<.SHOW SIDEBAR>*/
+
+/*<SIDEBAR SHOW>*/
+/* <Validate If Constant Exists> */
+/* <.Validate If Constant Exists> */
+/*<.SIDEBAR SHOW>*/
+
+/*<SIDEBAR HIDDEN>*/
+/* <Validate If Constant Exists> */
+/* <.Validate If Constant Exists> */
+/*<.SIDEBAR HIDDEN>*/
+
+/*<SKILLS TABS>*/
+/*<.SKILLS TABS>*/
+
+/*<MIXITUP FILTER PORTFOLIO>*/
+/*<.MIXITUP FILTER PORTFOLIO>*/
+
+/*<Link Active Work>*/
+/*<.Link Active Work>*/
+
+/*<Work Popup>*/
+/*<.Work Popup>*/
+
+/*<SERVICES MODAL>*/
+/*<.SERVICES MODAL>*/
+
+/*<SWIPER TESTIMONIAL>*/
+/*<.SWIPER TESTIMONIAL>*/
+
+/*<INPUT ANIMATION>*/
+/*<.INPUT ANIMATION>*/
+
+/*<SCROLL SECTIONS ACTIVE LINK>*/
+/*<.SCROLL SECTIONS ACTIVE LINK>*/
+
+/*<SHOW SCROLL UP>*/
+/*<.SHOW SCROLL UP>*/
